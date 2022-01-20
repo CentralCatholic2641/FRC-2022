4f0b58c3bab9fbfec1b5d8e96d4305a41b55c4ca
FRC <no.mail.address@o365.microsoft.com>
2022-01-20 16:02:15 -0500

    2022 drivetrain code

---
+++ src/main/java/frc/robot/commands/DrivingCommand.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DrivingCommand extends CommandBase {
+
+  public DrivingCommand() {
+    addRequirements(Robot.drivingSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+    double joystick1 = Robot.robotContainer.gamepad1.getRawAxis(Constants.joystick1);
+    double joystick2 = Robot.robotContainer.gamepad1.getRawAxis(Constants.joystick2);
+    Robot.drivingSubsystem.oDrive(joystick1, joystick2);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
